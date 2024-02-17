2fe8436b22c81954dd8e8f3019af08191975b81a
jhtigcal <jhtigcal@gmail.com>
2024-02-17 22:22:58 +0800

    feat: added tiny-mce editor page

---
+++ components/editors/tiny-mce-editor/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Editor } from "@tinymce/tinymce-react";
+
+export function TinyMceEditor() {
+  return (
+    <Editor
+      apiKey="yeyinuqe6xjo3vyr4am0mm79f1j1vsbh9nmywr9vko6j2c2s"
+      init={{
+        plugins:
+          "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss",
+        toolbar:
+          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+        tinycomments_mode: "embedded",
+        tinycomments_author: "Author name",
+        mergetags_list: [
+          { value: "First.Name", title: "First Name" },
+          { value: "Email", title: "Email" },
+        ],
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
+  );
+}
